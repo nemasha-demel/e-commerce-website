21461a87b2b1d7064ac27cc4776ddfc82efc11dd
nemasha-demel <nemashahirushidemel@gmail.com>
2025-06-12 01:55:01 +0530

    Add HeroGrid component

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-import './App.css'
 import Navigation from './components/Navigation'
+import HeroGrid from './components/HeroGrid'
 
 function App() {
 
   return (
     <>
      <Navigation/>
+     <HeroGrid/>
     </>
   )
 }
