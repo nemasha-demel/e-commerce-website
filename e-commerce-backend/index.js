3b71dddceddad8dee584b458bd58b151869f946e
nemasha-demel <nemashahirushidemel@gmail.com>
2025-06-21 22:14:11 +0530

    Initial server setup and port configuration

---
+++ e-commerce-backend/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 8000;
+
+app.listen(PORT,()=>{
+    console.log(`Server running at http://localhost:${PORT}`);
+})
