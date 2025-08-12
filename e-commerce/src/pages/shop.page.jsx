d4dae9a68b7a599765b02515a3815687630325b2
nemasha-demel <nemashahirushidemel@gmail.com>
2025-08-12 13:11:08 +0530

    Enhanced ShopPage to utilize a new API query for fetching products.

--- e-commerce/src/pages/shop.page.jsx
+++ e-commerce/src/pages/shop.page.jsx
@@ -1,34 +1,29 @@
-import React from 'react'
-import { useParams } from 'react-router'
-import { useEffect, useState } from 'react';
-import { getAllProducts } from '@/lib/products';
-
+import { useGetAllProductsQuery } from "@/lib/api";
 
 function ShopPage() {
-    const {category} = useParams();
-    const [products, setProducts] = useState([]);
-    const [error, setError] = useState("");
-    const [isloading, setIsLoading] = useState(true);
+  const {
+    data: products,
+    isLoading,
+    isError,
+    error,
+  } = useGetAllProductsQuery();
 
-    useEffect(() => {
-      getAllProducts(category)
-        .then((data) => setProducts(data))
-        .catch((error) => setError(error.message))
-        .finally(() => setIsLoading(false));
-    }, [category]);
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  console.log(products);
+  
 
   return (
-    <div>
-        <div>ShopPage</div>
-        <p>{category}</p>
-
-        <div>{isloading ? "Loading" : "Done"}</div>
-        <div>{error}</div>
-        <div>{JSON.stringify(products)}</div>
-    </div>
-    
-    
-  )
+    <main>
+      <h1>Shop Page</h1>
+      {/* <p>{category}</p> */}
+      <div>{isLoading ? "Loading" : "Done"}</div>
+      <div>{error}</div>
+      <div>{JSON.stringify(products)}</div>
+    </main>
+  );
 }
 
-export default ShopPage
+export default ShopPage;
