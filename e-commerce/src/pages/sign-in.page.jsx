ab572c917f80c966002772e935ad6c53a3ffb2a5
nemasha-demel <nemashahirushidemel@gmail.com>
2025-09-17 23:27:16 +0530

    Refactor layout styles for sign-in and sign-up pages; update font import in CSS

--- e-commerce/src/pages/sign-in.page.jsx
+++ e-commerce/src/pages/sign-in.page.jsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/clerk-react"
 
 function SignInPage() {
   return (
-    <main className="flex justify-center min-h-screen">
+    <main className="flex justify-center items-center min-h-screen">
       <SignIn/>
     </main>
   
--- e-commerce/src/pages/sign-up.page.jsx
+++ e-commerce/src/pages/sign-up.page.jsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/clerk-react"
 
 function SignUpPage() {
   return (
-    <main className="flex top-6justify-center min-h-screen">
+    <main className="flex justify-center items-center min-h-screen">
       <SignUp/>
     </main>
   )
